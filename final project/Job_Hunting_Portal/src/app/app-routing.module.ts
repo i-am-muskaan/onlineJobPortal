57d745268faefc008c54abd010c5da1b2b39afac
801935 <muskanmahajan62@gmail.com>
2022-08-01 14:29:40 +0530

    Team 4

---
+++ final project/Job_Hunting_Portal/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NoAuthGuard } from './guard/no-auth.guard';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent  ,canActivate:[NoAuthGuard]},
+  { path: 'register', component: SignupComponent,canActivate:[NoAuthGuard] },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
